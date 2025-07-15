6d7b16ec52c4e2c32eec0d62a5347172ef2d5e0c
Conway-eng <Mornah188@gmail.com>
2025-07-15 13:23:36 +0300

    Create Register.js

---
+++ components/Register.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:3001/api/auth/register', { username, password });
+      window.location.href = '/login';
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Register;
